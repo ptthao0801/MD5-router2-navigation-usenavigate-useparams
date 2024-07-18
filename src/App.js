b98eca1f02ab58cec8cc4f0a9ccb5faed032d0fc
ptthao0801 <ptthao0801@gmail.com>
2024-07-18 17:31:02 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Category from "./components/Category";
+import Product from "./components/Product";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>Please choose</h1>
+        <hr/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Category />} />
+                <Route path="/product/:categoryId" element={<Product />} />
+            </Routes>
+        </BrowserRouter>
+        </>
+        
+    );
 }
-
 export default App;
